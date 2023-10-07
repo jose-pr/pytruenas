cd416038860304064888ac702c778aa24e3d0f16
root <root@tnsbuilds01.tn.ixsystems.net>
2023-10-07 00:31:27 -0400

    Proof of Concept

---
+++ lib/pytruenas/_utils.py
@@ -0,0 +1,39 @@
+
+from typing import (
+    TypedDict as _Dict,
+    TypeVar as _TypeVar,
+    NamedTuple as _NT,
+    Sequence as _Sequence,
+    Mapping as _Map,
+    Generic as _Generic
+)
+
+def str_(txt: "bytes|str") -> str:
+    if isinstance(txt, str):
+        return txt
+    return txt.decode()
+
+
+def bytes_(txt: "bytes|str") -> bytes:
+    if isinstance(txt, str):
+        return txt.encode()
+    return txt
+
+def merge(*partials:_Map,**partial):
+    merged = {}
+    for partial in [*partials, partial]:
+        if partial:
+            merged.update(partial)
+    return merged
+
+class _Missing():
+    ...
+
+MISSING = _Missing
+
+def diff(base:_Map, against:_Map):
+    d = {}
+    for k, v in against.items():
+        if base.get(k, MISSING) != v:
+            d[k] = v
+    return d
