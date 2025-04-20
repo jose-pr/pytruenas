8fc4c0d691a4053493f2ad1334cfc7dab48fb36a
Jose A <jose-pr@coqui.dev>
2025-04-20 04:40:28 -0400

    run commands

--- lib/example.py
+++ lib/example.py
@@ -9,7 +9,7 @@ handler = logging.StreamHandler(sys.stderr)
 logging.getLogger().addHandler(handler)
 
 
-query = sqlutils.filter_from_kwargs(test=1, bob='test', bob2=sqlutils.EQ('equalto'))
+query = sqlutils.filter_from_kwargs(test=1, bob="test", bob2=sqlutils.EQ("equalto"))
 print(query)
 
 tn_host = os.environ.get("TN_HOST")
@@ -17,9 +17,17 @@ tn_creds = Credentials.from_env()
 client = TrueNASClient(tn_host, tn_creds, sslverify=False)
 client.logger.setLevel(logging.TRACE)
 
+result = TrueNASClient().run(
+    ("echo", "'\"text\"'"),
+    ("ls", "-l", "/mnt"),
+    "echo test",
+    capture_output=True,
+    encoding="utf8",
+)
+print(result.stdout)
+
 state = client.api.directoryservices.status()
-users = client.api.user._query(username=sqlutils.RE('adm.*'))
-admin = client.api.user._get(username='admin')
+users = client.api.user._query(username=sqlutils.RE("adm.*"))
+admin = client.api.user._get(username="admin")
 print(users)
 print(admin)
-
