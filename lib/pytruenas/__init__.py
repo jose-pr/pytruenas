06ef9e3e1f6031603fb3953bc1700d5c9c4e89a3
Jose A <jose-pr@coqui.dev>
2025-04-21 18:39:41 -0400

    Rename base file

--- lib/pytruenas/__init__.py
+++ lib/pytruenas/__init__.py
@@ -2,6 +2,6 @@ import logging as _logging
 from . import _utils
 
 _utils.add_logging_level("trace", _logging.DEBUG - 5)
-from .base import TrueNASClient
+from .client import TrueNASClient
 from .namespace import Namespace
 from .auth import *
