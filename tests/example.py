a5fcd583453ea076f6fb061b0611cfa735bbea09
Jose A <jose-pr@coqui.dev>
2025-04-22 02:09:49 -0400

    cleanup

--- tests/example.py
+++ tests/example.py
@@ -1,20 +1,15 @@
 #!/bin/python3
 from pytruenas import TrueNASClient, Credentials
-import typing
-if typing.TYPE_CHECKING:
-    from truenasapi_typings.current import Current
 import os, sys
 import logging
 from pytruenas.utils import query
-import json
-from pathlib import Path
 
 handler = logging.StreamHandler(sys.stderr)
 logging.getLogger().addHandler(handler)
 
 tn_host = os.environ.get("TN_HOST")
 tn_creds = Credentials.from_env()
-client = TrueNASClient['Current'](tn_host, tn_creds, sslverify=False)
+client = TrueNASClient(tn_host, tn_creds, sslverify=False)
 client.logger.setLevel(logging.TRACE)
 
 client.install_sshcreds()
