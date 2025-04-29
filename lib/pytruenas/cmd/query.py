98ce182d27c14b2c30a858c2d8d09a6c5ff17c0d
Jose A <jose-pr@coqui.dev>
2025-04-29 01:38:49 -0400

    Simple to use query command

---
+++ lib/pytruenas/cmd/query.py
@@ -0,0 +1,33 @@
+"""
+Get the supported api definitions from a TrueNAS host
+"""
+from pytruenas import TrueNASClient
+from pytruenas.utils.cmd import PyTrueNASArgs
+from logging import Logger
+import argparse
+
+import json
+
+class Args(PyTrueNASArgs):
+    query:list[str]
+    namespace: str
+
+def register(parser: argparse.ArgumentParser):
+    parser.add_argument('-q', '--query', action='append')
+    parser.add_argument('namespace')
+
+def run(client:TrueNASClient, args:Args, logger:Logger):
+    filter = {}
+    for entry in args.query:
+        if not entry:
+            continue
+        k,v = entry.split('=')
+        filter[k] = v
+
+
+    logger.info(f"Looking up object from {args.namespace}")
+
+    result = client.api[args.namespace]._query(**filter)
+
+    print(json.dumps(result))
+
