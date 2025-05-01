ee8f8cde4af6ed3173b5770ce3b1c83526581c6b
Jose A <jose-pr@coqui.dev>
2025-04-30 21:22:01 -0400

    remove colorama as dep for colored logs

---
+++ lib/example_cmds/create_local_sshkey.py
@@ -0,0 +1,41 @@
+"""
+Create a local ssh keypair for the host and add it to root authorizedkeys
+"""
+
+from pytruenas import TrueNASClient
+from pytruenas.utils.cmd import PyTrueNASArgs
+from logging import Logger
+import asyncssh as _ssh
+
+
+def run(client: TrueNASClient, args: PyTrueNASArgs, logger: Logger):
+
+    logger.info(f"Getting hostname from network configuration from {client._api.host}")
+    netconfig: dict[str, str] = client.api.network.configuration.config()
+    hostname: str = netconfig["hostname"]
+
+    keypair_name = hostname.upper()
+
+    logger.info(f"Get the current if any SSH Key Pair named: {keypair_name}")
+    keypair = client.api.keychaincredential._get(type="SSH_KEY_PAIR", name=keypair_name)
+    if not keypair:
+        logger.info(
+            f"Generating a SSH Key Pair as it currently doesnt exists"
+        )
+
+        private_key = client.api.keychaincredential.generate_ssh_key_pair()[
+            "private_key"
+        ]
+    else:
+        private_key = keypair["attributes"]["private_key"]
+
+    pubkey = _ssh.import_private_key(private_key).export_public_key().decode().strip()
+
+    logger.info(f"Upsert the current if any SSH Key Pair named")
+    keypair = client.api.keychaincredential._upsert(
+        ("name", "!type"),
+        (lambda x, id, result: logger.info(f"Action taken {x}, id of keypair: {id}")),
+        type="SSH_KEY_PAIR",
+        name=keypair_name,
+        attributes={"private_key": private_key, "public_key": pubkey},
+    )
