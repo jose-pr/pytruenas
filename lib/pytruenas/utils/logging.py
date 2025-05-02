604507113e7d0c647294b6b98197c7b6b8fdf4ca
Jose A <jose-pr@coqui.dev>
2025-05-01 22:33:31 -0400

    Clean up code

--- lib/pytruenas/utils/logging.py
+++ lib/pytruenas/utils/logging.py
@@ -1,11 +1,11 @@
 import logging as _logging
-from logging import *  # type:ignore
-
+import sys as _sys
 import typing as _ty
 
 
 if _ty.TYPE_CHECKING:
     import colorama as __coloroma
+    from logging import *  # type:ignore
 
 try:
     import colorama as _color  # type: ignore
@@ -15,10 +15,15 @@ except ImportError:
     _color = _ty.cast("__coloroma", None)
 
 
+def __getattr__(name: str):
+    return getattr(_logging, name)
+
+
 def _asicode(*codes):
     return "".join(["\033[" + str(c) + "m" for c in codes])
 
-def _getcolor(color:str):
+
+def _getcolor(color: str):
     if color.isalpha() and _color:
         fore, back, *_ = color.split("+") + ["", ""]
         if fore:
@@ -28,8 +33,9 @@ def _getcolor(color:str):
         color = fore + back
 
     if color.isalpha():
-        return ''
+        return ""
     return color
+
 
 if not hasattr(_logging, "getLevelNamesMapping"):
     _logging.getLevelNamesMapping = lambda: _logging._nameToLevel.copy()
@@ -40,32 +46,31 @@ def add_logging_level(name: str, level: int, force=False, color: str | None = No
     if hasattr(_logging, name) and not force:
         return
     setattr(_logging, name, level)
-    addLevelName(level, name)
+    _logging.addLevelName(level, name)
 
     def log_logger(self: Logger, message, *args, **kwargs):
         if self.isEnabledFor(level):
             self._log(level, message, args, **kwargs)
 
     name = name.lower()
-    setattr(getLoggerClass(), name, log_logger)
+    setattr(_logging.getLoggerClass(), name, log_logger)
 
     def log_root(msg, *args, **kwargs):
-        log(level, msg, *args, **kwargs)
+        _logging.log(level, msg, *args, **kwargs)
 
     if color is not None:
         DefaultFormatter.COLORS[level] = _getcolor(color)
 
-
     setattr(_logging, name, log_root)
 
 
-class DefaultFormatter(Formatter):  # type:ignore
+class DefaultFormatter(_logging.Formatter):  # type:ignore
     COLORS: dict[int, str] = {
-        DEBUG: _asicode(34),  # color.Fore.BLUE
-        INFO: _asicode(32),  # _color.Fore.GREEN,
-        WARNING: _asicode(33),  # _color.Fore.YELLOW,
-        ERROR: _asicode(31),  # _color.Fore.RED,
-        CRITICAL: _asicode(31, 47),  # color.Fore.RED + _color.Back.WHITE,
+        _logging.DEBUG: _asicode(34),  # Fore.BLUE
+        _logging.INFO: _asicode(32),  # Fore.GREEN
+        _logging.WARNING: _asicode(33),  # Fore.YELLOW
+        _logging.ERROR: _asicode(31),  # Fore.RED
+        _logging.CRITICAL: _asicode(31, 47),  # Fore.RED + Back.WHITE
     }
     RESET_ALL = _asicode(0)
 
@@ -79,14 +84,11 @@ class DefaultFormatter(Formatter):  # type:ignore
         self._levelsize: int | None = None
         super().__init__(fmt, datefmt, style, validate)
 
-
     def format(self, record):
         record.levelname = record.levelname.center(_LEVELSIZE)
         color = self.COLORS.get(record.levelno, None)
         if color:
-            record.levelname = (
-                f"{color}{record.levelname}{self.RESET_ALL}"
-            )
+            record.levelname = f"{color}{record.levelname}{self.RESET_ALL}"
         return super().format(record)
 
 
@@ -94,10 +96,11 @@ VERBOSE_LEVELS: dict[int, list[str]] = {}
 VERBOSE_HELP = ""
 _LEVELSIZE = 4
 
+
 def initverbose():
     global VERBOSE_LEVELS, VERBOSE_HELP, _LEVELSIZE
 
-    for name, loglevel in getLevelNamesMapping().items():
+    for name, loglevel in _logging.getLevelNamesMapping().items():
         if not loglevel:
             continue
         aliases: list[str] = VERBOSE_LEVELS.setdefault(loglevel, [])
@@ -112,10 +115,10 @@ def initverbose():
     VERBOSE_HELP = ", ".join([aliases[0] for aliases in VERBOSE_LEVELS.values()])
 
 
-import sys as _sys
-
-
-def init_stderr_logging(name=None):
-    handler = StreamHandler(_sys.stderr)
-    getLogger(name).addHandler(handler)
+def init_stderr_logging(name=None, level: int | None = None):
+    add_logging_level("TRACE", _logging.DEBUG - 5, color=_asicode(36))
+    handler = _logging.StreamHandler(_sys.stderr)
+    logger = _logging.getLogger(name)
+    logger.addHandler(handler)
     handler.setFormatter(DefaultFormatter())
+    return logger
