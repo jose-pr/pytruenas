efaebff6f19da5ab4720949242157e34ce498a04
Jose A <jose-pr@coqui.dev>
2025-05-01 22:39:41 -0400

    formatting

--- lib/pytruenas/cmd/dump_api.py
+++ lib/pytruenas/cmd/dump_api.py
@@ -1,6 +1,7 @@
 """
 Get the supported api definitions from a TrueNAS host
 """
+
 from pytruenas import TrueNASClient
 from pytruenas.utils.cmd import PyTrueNASArgs
 from logging import Logger
@@ -8,10 +9,9 @@ from logging import Logger
 import json
 
 
-def run(client:TrueNASClient, args:PyTrueNASArgs, logger:Logger):
+def run(client: TrueNASClient, args: PyTrueNASArgs, logger: Logger):
 
     logger.info("Generating api at server")
     apidump = json.dumps(client.dump_api())
-
 
     print(apidump)
